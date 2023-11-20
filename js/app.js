280c333241c0dfede8ea198fdb118ba1be7776ca
Jontxu <jonburgos4@gmail.com>
2023-11-20 09:17:05 +0100

    validacion formulario

--- js/app.js
+++ js/app.js
@@ -30,3 +30,25 @@ document.addEventListener('DOMContentLoaded', () => {
     ui.llenarOpciones(); // Llena el select con los años
 
 })
+
+eventListeners();
+function eventListeners() {
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.addEventListener('submit', cotizarSeguro);
+};
+
+function cotizarSeguro(e) {
+
+    //Leer la marca
+    const marca = document.querySelector('#marca').value;
+    //leer el año
+    const year = document.querySelector('#year').value;
+    //leer tipo cobertura
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+    
+    if (marca === '' || year === '' || tipo === '') {
+        console.log('no paso la validacion')
+    } else {
+        console.log('si la paso')
+    }
+}
